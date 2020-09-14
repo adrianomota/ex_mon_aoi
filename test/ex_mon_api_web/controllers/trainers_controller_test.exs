fb09eeb850842c7cfc0b1fb50761f7b083f5bd5b
Adriano Mota <adrianowsh@hotmail.com>
2020-09-13 21:32:13 -0300

    fix credo strict

--- test/ex_mon_api_web/controllers/trainers_controller_test.exs
+++ test/ex_mon_api_web/controllers/trainers_controller_test.exs
@@ -7,7 +7,9 @@ defmodule ExMonApiWeb.Controller.TrainersControllerTest do
   describe "show/2" do
     setup %{conn: conn} do
       params = %{name: "trainer 1", password: "123456"}
+
       {:ok, trainer} = ExMonApi.create_trainer(params)
+
       {:ok, token, _claims} = Guardian.encode_and_sign(trainer)
 
       conn = put_req_header(conn, "authorization", "Bearer #{token}")
