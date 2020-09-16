8cb5eea562df34adc8ef22c1dd19f2d77313dafc
Adriano Mota <adrianowsh@hotmail.com>
2020-09-16 13:04:46 -0300

    include credo

--- lib/ex_mon_api.ex
+++ lib/ex_mon_api.ex
@@ -10,9 +10,7 @@ defmodule ExMonApi do
   defdelegate delete_trainer(id), to: Trainer.Delete, as: :call
   defdelegate fetch_trainer(id), to: Trainer.Get, as: :call
   defdelegate update_trainer(params), to: Trainer.Update, as: :call
-
   defdelegate fetch_pokemon(name), to: Pokemon.Get, as: :call
-
   defdelegate fetch_trainer_pokemon(id), to: TrainerPokemon.Get, as: :call
   defdelegate create_trainer_pokemon(params), to: TrainerPokemon.Create, as: :call
   defdelegate delete_trainer_pokemon(id), to: TrainerPokemon.Delete, as: :call
